99e374d9fc48cdda89229084f257b2a09454b708
PrincessTG8815 <transgirl8815@yahoo.com>
2026-01-03 11:58:01 -0600

    path change

--- public/script/main.js
+++ public/script/main.js
@@ -2,9 +2,11 @@ import "./login/auth.js"
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 

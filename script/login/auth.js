35a2c0c22addcaa12f05bede944d47189bc61647
PrincessTG8815 <transgirl8815@yahoo.com>
2026-01-03 03:11:06 -0600

    auth

---
+++ script/login/auth.js
@@ -0,0 +1,48 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "firebase/auth";
+
+import { auth } from "../main.js";
+
+registerBtn.onclick = async () => {
+  try {
+    await createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+  } catch (err) {
+    authError.textContent = err.message;
+  }
+};
+
+loginBtn.onclick = async () => {
+  try {
+    await signInWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
+  } catch (err) {
+    authError.textContent = err.message;
+  }
+};
+
+onAuthStateChanged(auth, user => {
+  if (user) {
+    console.log("Logged in:", user.uid);
+
+    // Hide login UI
+    document.getElementById("authPanel").style.display = "none";
+
+    // Load player data
+    loadPlayer(user.uid);
+  } else {
+    console.log("Logged out");
+    document.getElementById("authPanel").style.display = "block";
+  }
+});
+
